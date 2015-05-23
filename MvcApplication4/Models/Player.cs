cf3bf9b03b6e0c08a72d04dfb88cab384ead9c40
LinJustin <justin@lccnet.com.tw>
2015-05-23 21:09:15 +0800

    +Model

---
+++ MvcApplication4/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcApplication4.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string CName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email {get; set;}
+        public string Mobile { get; set; }
+    }
+
+    public class PlayerDBContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+    }
+}
