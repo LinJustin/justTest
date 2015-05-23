da19368dee081d3017fcf80033b6fc3948de1645
LinJustin <justin@lccnet.com.tw>
2015-05-23 18:17:26 +0800

    +view

---
+++ MvcApplication4/Controllers/WhoamiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class WhoamiController : Controller
+    {
+        //
+        // GET: /Whoami/
+
+        public ActionResult Welcome(string name, int id = 1)
+        {
+            ViewBag.Message = name;
+            ViewBag.Id = id;
+            return View();
+
+            //return HttpUtility.HtmlEncode("Hello " + name + ", id: " + id);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
